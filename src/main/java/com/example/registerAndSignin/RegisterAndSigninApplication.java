ca1e8e734c98d8e8f7a0eddb4af2907dc6787d9d
Hemanth912 <116891140+Hemanth912@users.noreply.github.com>
2023-03-20 11:09:37 +0530

    1st commit

---
+++ src/main/java/com/example/registerAndSignin/RegisterAndSigninApplication.java
@@ -0,0 +1,13 @@
+package com.example.registerAndSignin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAndSigninApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAndSigninApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/registerAndSignin/securityConfig.java
@@ -0,0 +1,14 @@
+package com.example.registerAndSignin;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+
+public class securityConfig extends WebSecurityConfiguration {
+}
